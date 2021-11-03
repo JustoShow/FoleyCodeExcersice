703d49651a899a8678cbf7bcd2960a3ececada42
Justoshow <justoproductions@yahoo.com>
2021-11-03 09:12:31 -0500

    Add project files.

---
+++ FileParser/Parse.cs
@@ -0,0 +1,84 @@
+﻿using FileParser.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace FileParser
+{
+    public static class Parse
+    {
+        public static List<Person> GetPeopleFromCSV(
+            string filePath, bool hasHeader = true)
+        {
+            List<Person> people = new();
+            string[] peopleData = LoadCSV(filePath);
+            foreach (var person in peopleData)
+            {
+                if (hasHeader) 
+                {
+                    hasHeader = false;
+                    continue;
+                }
+                var line = SplitCSV(person);
+                people.Add(ParseToObject(line));
+            }
+
+            return people;
+        }
+
+        public static string[] LoadCSV(string filePath)
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                return lines;
+            }
+            catch (Exception)
+            {                
+                return null;
+            }
+        }
+
+        public static Person ParseToObject(List<string> line)
+        {
+            Person person = new()
+            {
+                Id = Convert.ToInt32(line[0]),
+                FirstName = line[1].Trim(),
+                LastName = line[2].Trim(),
+                Address = line[3].Trim()
+            };
+
+            return person;
+        }
+
+        private static readonly Regex csvSplit =
+                new Regex("(?:^|,)(\"(?:[^\"]+)*\"|[^,]*)");
+
+        public static List<string> SplitCSV(string line)
+        {
+            string cleanLine = line.Replace("\"\"", "");
+            List<string> splitLine = new();
+            string currentValue = null;
+            var matches = csvSplit.Matches(cleanLine);
+
+            foreach (Match match in matches)
+            {
+                currentValue = match.Value;
+
+                if (currentValue.Length == 0) 
+                { splitLine.Add(""); }
+
+                splitLine.Add(
+                    match.Value
+                    .Trim(',')
+                    .Replace('"', ' ')
+                    .Trim());
+            }
+
+            return splitLine; 
+        }
+    }
+}
---
+++ FileParserTest/ParseTest.cs
@@ -0,0 +1,117 @@
+﻿using FileParser;
+using FileParser.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileParserTest
+{
+    [TestClass]
+    [DeploymentItem(@"TestData\PeopleTest.csv", "TestData")]
+    public class ParseTest
+    {
+        [TestMethod]
+        public void Parse_GetPeopleFromCSV()
+        {
+            string fileName = "PeopleTest.csv";
+            string filePath =
+                Path.Combine(
+                    Environment.CurrentDirectory,
+                    @"TestData\", fileName);
+
+            int expected = 12;
+            var actual = Parse
+                .GetPeopleFromCSV(filePath)
+                .Count();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Parse_LoadCSVCheckByLineCount()
+        {
+            string fileName = "PeopleTest.csv";
+            string filePath = 
+                Path.Combine(
+                    Environment.CurrentDirectory, 
+                    @"TestData\", fileName);
+
+            int expected = 13;
+            int actual = Parse.LoadCSV(filePath).Length;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Parse_SplitCSV_TestCommasInString()
+        {
+            string csvRow = "2,\"Abe\",\"Testing\",\"199 Any Where Drive, Apartment E\"";
+            List<string> expected = new()
+            {
+                "2", 
+                "Abe", 
+                "Testing", 
+                "199 Any Where Drive, Apartment E"
+            };
+
+            var actual = Parse.SplitCSV(csvRow);
+
+            Assert.AreEqual(expected[0], actual[0]);
+            Assert.AreEqual(expected[1], actual[1]);
+            Assert.AreEqual(expected[2], actual[2]);
+            Assert.AreEqual(expected[3], actual[3]);
+        }
+
+        [TestMethod]
+        public void Parse_SplitCSV_TestDoubleQuotesInString()
+        {
+            string csvRow = "52,\"Rolo\",\"Candy\",\"600 \"\"Zebra Stripe\"\" Avenue";
+            List<string> expected = new()
+            {
+                "52",
+                "Rolo",
+                "Candy",
+                "600 Zebra Stripe Avenue"
+            };
+
+            var actual = Parse.SplitCSV(csvRow);
+
+            Assert.AreEqual(expected[0], actual[0]);
+            Assert.AreEqual(expected[1], actual[1]);
+            Assert.AreEqual(expected[2], actual[2]);
+            Assert.AreEqual(expected[3], actual[3]);
+        }
+
+        [TestMethod]
+        public void Parse_ParseToObject()
+        {
+            List<string> csvRow = new()
+            {
+                "7",
+                "Gabe",
+                "Testing",
+                "500 Abe Street"
+            };
+
+            Person expected = new()
+            {
+                Id = 7,
+                FirstName = "Gabe",
+                LastName = "Testing",
+                Address = "500 Abe Street"
+            };
+
+            var actual = Parse.ParseToObject(csvRow);
+
+            Assert.AreEqual(expected.Id, actual.Id);
+            Assert.AreEqual(expected.FirstName, actual.FirstName);
+            Assert.AreEqual(expected.LastName, actual.LastName);
+            Assert.AreEqual(expected.Address, actual.Address);
+        }
+    }
+}
