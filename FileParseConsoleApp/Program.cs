b3fe8b8bcd6c2e1d429b66688fa95219b2d25239
Justoshow <justoproductions@yahoo.com>
2021-11-03 09:47:28 -0500

    Added Console App

---
+++ FileParseConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using FileParser;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace FileParseConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = "PeopleTest.csv";
+
+            if (!File.Exists(fileName)) 
+            {
+                Console.WriteLine("File does not exist.");
+                return;
+            }
+
+            var people = Parse
+                .GetPeopleFromCSV(fileName);
+
+            foreach (var person in people.OrderBy(p => p.LastName))
+            {
+                Console.WriteLine("Id: {0}\t " +
+                    "Last Name: {1}\t " +
+                    "First Name: {2}\t " +
+                    "Address: {3}",
+                    person.Id,
+                    person.LastName,
+                    person.FirstName,
+                    person.Address);
+            }
+
+        }
+    }
+}
